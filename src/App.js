384d7e0a08ede1dcdd564f4f76b324fb830ee1a3
manato kuroda <a7108100@gmail.com>
2018-09-29 21:19:34 +0900

    chore: add babel test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  testPipeline() {
+    const fn = val => `${val} world`
+    return 'hello' |> fn
+  }
+
+  testDo() {
+    return do {
+      const test = 'hi'
+      test
+    }
+  }
+
   render() {
+    this.testPipeline()
+    this.testDo()
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+          />
+          <h1 className="App-title">
+            Welcome to React
+          </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit{' '}
+          <code>src/App.js</code> and save to
+          reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
